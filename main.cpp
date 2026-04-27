17946164a47cea023ff937d5c2b4fe62049d7b9c
Vladimir <vova15233@mail.ru>
2026-04-27 18:47:42 +0700

    Добавлена базовая логика состояния файла

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <QCoreApplication>
 #include <QTextStream>
+#include <QString>
 
 int main(int argc, char *argv[])
 {
@@ -10,5 +11,26 @@ int main(int argc, char *argv[])
     out << "Lab_1 File Watcher started" << Qt::endl;
     out << "Qt console application" << Qt::endl;
     out << "Project setup step" << Qt::endl;
+
+    QString filePath = "empty.txt";
+    bool exists = true;
+    qint64 size = 125;
+
+    out << "filePath: " << filePath << Qt::endl;
+
+    if (exists) {
+        out << "File exists" << Qt::endl;
+        if (size == 0) {
+            out << "File is empty" << Qt::endl;
+        }
+        else {
+            out << "File is not empty" << Qt::endl;
+            out << "Size: " << size << " bytes" << Qt::endl;
+        }
+           
+    }
+    else {
+        out << "File does not exist" << Qt::endl;
+    }
     return 0;
 }
