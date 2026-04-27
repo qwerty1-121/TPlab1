1f19d640a22d4354e416a6e87b991bacb12842a1
Vladimir <vova15233@mail.ru>
2026-04-27 22:53:39 +0700

    Добавлена проверка файла через QFileInfo

--- main.cpp
+++ main.cpp
@@ -1,20 +1,25 @@
 #include <QCoreApplication>
 #include <QTextStream>
 #include <QString>
+#include <QFileInfo>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
 
+    QTextStream in(stdin);
     QTextStream out(stdout);
 
     out << "Lab_1 File Watcher started" << Qt::endl;
     out << "Qt console application" << Qt::endl;
     out << "Project setup step" << Qt::endl;
 
-    QString filePath = "empty.txt";
-    bool exists = true;
-    qint64 size = 125;
+    out << "Enter file path: " << Qt::endl;
+    QString filePath = in.readLine();
+    QFileInfo fileInfo(filePath);
+
+    bool exists = fileInfo.exists();
+    qint64 size = fileInfo.size();
 
     out << "filePath: " << filePath << Qt::endl;
 
