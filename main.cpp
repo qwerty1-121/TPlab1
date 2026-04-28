9cb99cdabc026d5d6491f729e7dba82c83d23995
Vladimir <vova15233@mail.ru>
2026-04-28 14:18:21 +0700

    Добавлено сравнение состояний файла

--- main.cpp
+++ main.cpp
@@ -16,26 +16,69 @@ int main(int argc, char *argv[])
 
     out << "Enter file path: " << Qt::endl;
     QString filePath = in.readLine();
-    QFileInfo fileInfo(filePath);
+    QFileInfo oldFileInfo(filePath);
 
-    bool exists = fileInfo.exists();
-    qint64 size = fileInfo.size();
+    bool oldExists = oldFileInfo.exists();
+    qint64 oldSize = oldFileInfo.size();
 
     out << "filePath: " << filePath << Qt::endl;
+    out << "Old state:" << Qt::endl;
 
-    if (exists) {
+    if (oldExists) {
         out << "File exists" << Qt::endl;
-        if (size == 0) {
+        if (oldSize == 0) {
             out << "File is empty" << Qt::endl;
         }
         else {
             out << "File is not empty" << Qt::endl;
-            out << "Size: " << size << " bytes" << Qt::endl;
+            out << "Size: " << oldSize << " bytes" << Qt::endl;
         }
            
     }
     else {
         out << "File does not exist" << Qt::endl;
     }
+
+    out << "Change the file and press Enter..." << Qt::endl;
+    in.readLine();
+    QFileInfo newFileInfo(filePath);
+
+    bool newExists = newFileInfo.exists();
+    qint64 newSize = newFileInfo.size();
+
+    out << "New state:" << Qt::endl;
+
+    if (newExists) {
+        out << "File exists" << Qt::endl;
+
+        if (newSize == 0) {
+            out << "File is empty" << Qt::endl;
+        }
+        else {
+            out << "File is not empty" << Qt::endl;
+            out << "Size: " << newSize << " bytes" << Qt::endl;
+        }
+    }
+    else {
+        out << "File does not exist" << Qt::endl;
+    }
+
+    out << "Event:" << Qt::endl;
+
+    if (oldExists == false && newExists == true) {
+        out << "File appeared" << Qt::endl;
+    }
+    else if (oldExists == true && newExists == false) {
+        out << "File disappeared" << Qt::endl;
+    }
+    else if (oldExists == true && newExists == true && oldSize != newSize) {
+        out << "File size changed" << Qt::endl;
+        out << "Old size: " << oldSize << " bytes" << Qt::endl;
+        out << "New size: " << newSize << " bytes" << Qt::endl;
+    }
+    else {
+        out << "No changes" << Qt::endl;
+    }
     return 0;
 }
+    
